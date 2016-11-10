d5f9c400601004b541bb8301af5fe85e504e07b8
anonymous <caoxuanquy1985@gmail.com>
2016-11-10 15:13:04 +0700

    new commit

--- src/Example/Example4.java
+++ src/Example/Example4.java
@@ -4,14 +4,17 @@ package Example;
  * Created by anonymous on 11/10/2016.
  */
 import innerclassess.*;
+import typeinfo.pets.*;
 
 public class Example4 {
     public static void main(String[] args){
-        Sequence<Integer> sequence = new Sequence<>();
-        for(int i = 1; i < 10; i++){
-            sequence.add(i);
+        LiteralPetCreator creator = new LiteralPetCreator();
+        Pet[] pets = creator.createArray(10);
+        Sequence<Pet> sequence = new Sequence<Pet>();
+        for(int i = 0; i < pets.length; i++){
+            sequence.add(pets[i]);
         }
-        Selector<Integer> selector = sequence.selector();
+        Selector<Pet> selector = sequence.selector();
         while(!selector.end()){
             System.out.print(selector.chooseCurrent() + " ");
             selector.next();
