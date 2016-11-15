0f8e00bc4b368e6264d7bd2db5c3730805732ecf
Dell Vostro 5459 <caoxuanquy1985@gmail.com>
2016-11-16 00:46:48 +0700

    new commit

--- src/generics/Generators.java
+++ src/generics/Generators.java
@@ -5,14 +5,14 @@ import java.util.*;
  * Created by anonymous on 11/15/2016.
  */
 public class Generators {
-    public static <T> Collection<T> collection(Collection<T> coll, Generator<T> gen, int n){
+    public static <T> Collection<T> fill(Collection<T> coll, Generator<T> gen, int n){
         for(int i = 0; i < n; i++){
             coll.add(gen.next());
         }
         return coll;
     }
     public static void main(String[] args){
-        Collection<Coffee> list = collection(new ArrayList<Coffee>(), new CoffeeGenerator(), 10);
+        Collection<Coffee> list = fill(new ArrayList<Coffee>(), new CoffeeGenerator(), 10);
         System.out.println(list);
     }
 }
