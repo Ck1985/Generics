c1a02a775436ff358ede2bba2af31e64be00ef60
anonymous <caoxuanquy1985@gmail.com>
2016-11-15 14:11:06 +0700

    new commit

---
+++ src/util/New.java
@@ -0,0 +1,29 @@
+package util;
+
+/**
+ * Created by anonymous on 11/15/2016.
+ */
+import java.util.*;
+
+public class New {
+    public static <K, V> Map<K, V> map(){
+        return new HashMap<K, V>();
+    }
+    public static <T> List<T> list(){
+        return new ArrayList<T>();
+    }
+    public static <T> LinkedList<T> lList(){
+        return new LinkedList<T>();
+    }
+    public static <T> Set<T> set(){
+        return new HashSet<T>();
+    }
+    public static <T> Queue<T> queue(){
+        return new LinkedList<T>();
+    }
+    public static void main(String[] args){
+        Map<String, List<Integer>> map = New.map();
+        List<Double> list = New.list();
+        LinkedList<Float> iList = New.lList();
+    }
+}
