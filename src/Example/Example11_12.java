ca8cd7d1a547865f11dbb6b8c0701c4e300b9332
anonymous <caoxuanquy1985@gmail.com>
2016-11-15 15:59:48 +0700

    new commit

--- src/Example/Example11_12.java
+++ src/Example/Example11_12.java
@@ -11,8 +11,11 @@ class Class_1{
 class Class_2{
 
 }
-public class Example11 {
+public class Example11_12 {
+    public void f(Map<Class_1, List<Class_2>> petPerson){}
     public static void main(String[] args){
         Map<Class_1, List<? extends Class_2>> mapList = New.map();
+        Example11_12 example11_12 = new Example11_12();
+        example11_12.f(New.<Class_1, List<Class_2>>map());
     }
 }
