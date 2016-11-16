99d117428bca51c52707b171a30137ed23e4daa9
anonymous <caoxuanquy1985@gmail.com>
2016-11-16 14:58:39 +0700

    new commit

---
+++ src/generics/TupleTest2.java
@@ -0,0 +1,33 @@
+package generics;
+
+/**
+ * Created by anonymous on 11/16/2016.
+ */
+import util.*;
+public class TupleTest2 {
+    public static TwoTuple<String, Integer> f(){
+        return Tuple.tuple("Hi", 47);
+    }
+    public static ThreeTuple<Amphibian, String, Integer> k(){
+        return Tuple.tuple(new Amphibian(), "Hi", 47);
+    }
+    public static FourTuple<Vehicle, Amphibian, String, Integer> h(){
+        return Tuple.tuple(new Vehicle(), new Amphibian(), "Hi", 47);
+    }
+    public static FiveTuple<Amphibian, Vehicle, Amphibian, String, Integer> j(){
+        return Tuple.tuple(new Amphibian(), new Vehicle(), new Amphibian(), "Hi", 47);
+    }
+    public static void main(String[] args){
+        System.out.println(f());
+        System.out.println(k());
+        System.out.println(h());
+        System.out.println(j());
+
+
+
+
+
+
+
+    }
+}
---
+++ src/util/Tuple.java
@@ -0,0 +1,21 @@
+package util;
+
+/**
+ * Created by anonymous on 11/16/2016.
+ */
+import generics.*;
+
+public class Tuple {
+    public static <T, K> TwoTuple<T, K> tuple(T t, K k){
+        return new TwoTuple<T, K>(t, k);
+    }
+    public static <T, K, U> ThreeTuple<T, K, U> tuple(T t, K k, U u){
+        return new ThreeTuple<T, K, U>(t, k, u);
+    }
+    public static <T, K, U, V> FourTuple<T, K, U, V> tuple(T t, K k, U u, V v){
+        return new FourTuple<T, K, U, V>(t, k, u, v);
+    }
+    public static <T, K, U, V, I> FiveTuple<T, K, U, V, I> tuple(T t, K k, U u, V v, I i){
+        return new FiveTuple<T, K, U, V, I>(t, k, u, v, i);
+    }
+}
