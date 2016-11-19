1cd73247841894dab6336a07fdf8353ec945adb9
Dell Vostro 5459 <caoxuanquy1985@gmail.com>
2016-11-19 10:50:58 +0700

    new commit

--- src/generics/LostInformation.java
+++ src/generics/LostInformation.java
@@ -7,11 +7,17 @@ import java.util.*;
 
 class Frob{}
 class Fnorkle{}
-class Quark{}
+class Quark<Q>{}
 class Pacticle<POSITION, MOMENTIUM>{}
 public class LostInformation {
     public static void main(String[] args){
         List<Frob> list = new ArrayList<Frob>();
         Map<Frob, Fnorkle> map = new HashMap<Frob, Fnorkle>();
+        Quark<Fnorkle> quark = new Quark<Fnorkle>();
+        Pacticle<Long, Double> p = new Pacticle<>();
+        System.out.println(Arrays.toString(list.getClass().getTypeParameters()));
+        System.out.println(Arrays.toString(map.getClass().getTypeParameters()));
+        System.out.println(Arrays.toString(quark.getClass().getTypeParameters()));
+        System.out.println(Arrays.toString(p.getClass().getTypeParameters()));
     }
 }
