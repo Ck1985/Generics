02b6542237ad57bd79c743f4c32f89ec0ebbe30f
Dell Vostro 5459 <caoxuanquy1985@gmail.com>
2016-11-26 12:27:11 +0700

    new commit

--- src/generics/Erased.java
+++ src/generics/Erased.java
@@ -3,5 +3,12 @@ package generics;
 /**
  * Created by Dell Vostro 5459 on 11/26/2016.
  */
-public class Erased {
+public class Erased<T> {
+    private static final int SIZE = 100;
+    public void f(Object arg){
+         // if(arg instanceof T){} Error
+        // T t = new T(); error
+        // T[] arrayT = new T[SIZE];Error
+         T[] array = (T[])new Object[SIZE]; // Warning check
+    }
 }
