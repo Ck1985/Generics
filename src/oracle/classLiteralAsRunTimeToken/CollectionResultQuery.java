501e8dec5ae1dc136cf9044ebd5fe0d24aa7e8de
cunkin1985 <anonymous.vn1985@gmail.com>
2016-12-01 15:27:04 +0700

    new commit

---
+++ src/oracle/classLiteralAsRunTimeToken/CollectionResultQuery.java
@@ -0,0 +1,21 @@
+package oracle.classLiteralAsRunTimeToken;
+
+/**
+ * Created by anonymous on 12/1/2016.
+ */
+import java.util.*;
+
+public class CollectionResultQuery {
+    public <T> Collection<T> select(Factory<T> factory, String statement){
+        Collection<T> resultCollection = new ArrayList<>();
+        /*Run sql query using jdbc*/
+       // for(/*iterator over jdbc result*/){
+            T item = factory.make();
+        resultCollection.add(item);
+       // }
+        return resultCollection;
+    }
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/oracle/classLiteralAsRunTimeToken/Factory.java
@@ -0,0 +1,8 @@
+package oracle.classLiteralAsRunTimeToken;
+
+/**
+ * Created by anonymous on 12/1/2016.
+ */
+public interface Factory<T> {
+    public T make();
+}
