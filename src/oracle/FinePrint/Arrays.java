ec2edff52efeed73aef55bdf58f316c9312571ff
cunkin1985 <anonymous.vn1985@gmail.com>
2016-12-01 10:41:58 +0700

    new commit

--- src/oracle/FinePrint/Arrays.java
+++ src/oracle/FinePrint/Arrays.java
@@ -6,6 +6,9 @@ package oracle.FinePrint;
 import java.util.*;
 
 public class Arrays {
+    /*public <T> T[] createGenericArray(){
+        return new T[100];
+    }*/
     public static void main(String[] args){
         //Not really allowed
         /*List<String>[] arrayStringList = new List<String>[10];
@@ -16,5 +19,16 @@ public class Arrays {
         objectArray[1] = integerList;
         // Run time error:
         String string = arrayStringList[1].get(0);*/
+
+        //Generic Array is not allowed, but Wildcard array is still OK
+        List<?>[] lists = new ArrayList<?>[10];
+        Object object = lists;
+        Object[] objectArray = (Object[])object;
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(new Integer(10));
+        objectArray[1] = integerList;
+        String string = (String)lists[1].get(0); // OK cast explecit
+
+        // List<String>[] lists1 = new ArrayList<?>[10];
     }
 }
