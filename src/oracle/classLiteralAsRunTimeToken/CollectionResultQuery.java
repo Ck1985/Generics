6370e31576fe41005ca9f29ff5ebde858db84ae6
cunkin1985 <anonymous.vn1985@gmail.com>
2016-12-02 11:07:44 +0700

    new commit

--- src/oracle/classLiteralAsRunTimeToken/CollectionResultQuery.java
+++ src/oracle/classLiteralAsRunTimeToken/CollectionResultQuery.java
@@ -5,9 +5,14 @@ package oracle.classLiteralAsRunTimeToken;
  */
 import java.util.*;
 
-
+class EmpInfo{}
+class EmpFactory implements Factory<EmpInfo>{
+    public EmpInfo make(){
+        return new EmpInfo();
+    }
+}
 public class CollectionResultQuery {
-    public <T> Collection<T> select(Factory<T> factory, String statement){
+    public static <T> Collection<T> select(Factory<T> factory, String statement){
         Collection<T> resultCollection = new ArrayList<>();
         /*Run sql query using jdbc*/
        // for(/*iterator over jdbc result*/){
@@ -16,7 +21,42 @@ public class CollectionResultQuery {
        // }
         return resultCollection;
     }
+    // select method uses Class literal
+    // Without Generic
+    public static Collection select2(Class objectClass, String sqlStatement){
+        Collection resultQuery = new ArrayList();
+        // Run sql query using jdbc
+        //for(/*Iterator over jdbc result*/){
+           try {
+               Object item = objectClass.newInstance();
+               resultQuery.add(item);
+           }catch(Exception e){
+               throw new RuntimeException(e);
+           }
+
+        //}
+        return resultQuery;
+    }
+    public static <T> Collection<T> select3(Class<T> tClass, String sqlStatement){
+        Collection<T> resultQuery = new ArrayList<>();
+        //for(){
+        try {
+            T t = tClass.newInstance();
+            resultQuery.add(t);
+        }catch(Exception e){
+
+        }
+        //}
+        return resultQuery;
+    }
     public static void main(String[] args){
+        Collection<EmpInfo> resultQuery = select(new Factory<EmpInfo>(){
+            public EmpInfo make(){
+                return new EmpInfo();
+            }
+        }, "query statement");
+        Collection<EmpInfo> resultQuery2 = select(new EmpFactory(), "query statement");
+        // Using Class literal:
 
     }
 }
