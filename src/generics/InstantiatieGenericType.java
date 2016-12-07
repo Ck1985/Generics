9cab99560a011ac455ca2aef0514ab89fbe74d89
Dell Vostro 5459 <caoxuanquy1985@gmail.com>
2016-12-07 23:14:17 +0700

    new commit

--- src/generics/InstantiatieGenericType.java
+++ src/generics/InstantiatieGenericType.java
@@ -17,5 +17,11 @@ class ClassAsFactory<T>{
 public class InstantiatieGenericType {
     public static void main(String[] args){
         ClassAsFactory<Employee2> fe = new ClassAsFactory<Employee2>(Employee2.class);
+        System.out.println("ClassAsFactory<Employee2> successed !");
+        try {
+            ClassAsFactory<Integer> fi = new ClassAsFactory<>(Integer.class);
+        }catch(Exception e){
+            System.out.println("ClassAsFactory<Integer> failed !!!");
+        }
     }
 }
