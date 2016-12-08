d26eb74c99084b9f43975a52a060d66a17613d5b
cunkin1985 <anonymous.vn1985@gmail.com>
2016-12-08 08:26:06 +0700

    new commit

--- src/generics/FactoryConstraint.java
+++ src/generics/FactoryConstraint.java
@@ -12,8 +12,21 @@ class Foo2<T>{
         x = factory.create();
     }
 }
+class FactoryInteger implements FactoryI<Integer>{
+    public Integer create(){
+        return new Integer(0);
+    }
+}
+class Widget {
+    public static class Factory implements FactoryI<Widget>{
+        public Widget create(){
+            return new Widget();
+        }
+    }
+}
 public class FactoryConstraint {
     public static void main(String[] args){
-
+        new Foo2<Integer>(new FactoryInteger());
+        new Foo2<Widget>(new Widget.Factory());
     }
 }
