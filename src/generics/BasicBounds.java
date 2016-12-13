e2f8fbc575c0ecf17cb18a258924fb5d408a84eb
Dell Vostro 5459 <caoxuanquy1985@gmail.com>
2016-12-13 11:23:59 +0700

    new commit

--- src/generics/BasicBounds.java
+++ src/generics/BasicBounds.java
@@ -21,13 +21,64 @@ class Colored<T extends HasColor>{
         return this.item.getColor();
     }
 }
-class Dimension{}
-class ColoredDimension{}
-interface Weight{}
-class Solid{}
-class Bounded{}
+class Dimension{
+    public int x, y, z;
+}
+class ColoredDimension<T extends Dimension & HasColor>{
+    T item;
+    ColoredDimension(T item){
+        this.item = item;
+    }
+    T getItem(){
+        return this.item;
+    }
+    Color color(){
+        return this.item.getColor();
+    }
+    int getX(){
+        return this.item.x;
+    }
+    int getY(){
+        return this.item.y;
+    }
+    int getZ(){
+        return this.item.z;
+    }
+}
+interface Weight{
+    int getWeight();
+}
+class Solid<T extends Dimension & HasColor & Weight>{
+    T item;
+    Solid(T item){
+        this.item = item;
+    }
+    T getItem(){
+        return this.item;
+    }
+    Color color(){
+        return this.item.getColor();
+    }
+    int getX(){return this.item.x;}
+    int getY(){return this.item.y;}
+    int getZ(){return this.item.z;}
+    int weight(){
+        return this.item.getWeight();
+    }
+}
+class Bounded extends Dimension implements HasColor, Weight{
+    public Color getColor(){
+        return null;
+    }
+    public int getWeight(){
+        return 0;
+    }
+}
 public class BasicBounds {
     public static void main(String[] args){
-
+        Solid<Bounded> solid = new Solid<>(new Bounded());
+        solid.color();
+        solid.getY();
+        solid.weight();
     }
 }
