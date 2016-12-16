bc42fe0f7f81d9a857241b2a62b071ca7247128b
cunkin1985 <anonymous.vn1985@gmail.com>
2016-12-16 17:29:41 +0700

    new commit

---
+++ src/generics/UnboundedWildcards1.java
@@ -0,0 +1,33 @@
+package generics;
+
+/**
+ * Created by anonymous on 12/16/2016.
+ */
+import java.util.*;
+
+public class UnboundedWildcards1 {
+    static List list1;
+    static List<?> list2;
+    static List<? extends Object> list3;
+    public static void assign1(List list){
+        list1 = list;
+        list2 = list;
+        list3 = list; // Warning: Unchecked
+    }
+    public static void assign2(List<?> list){
+        list1 = list;
+        list2 = list;
+        list3 = list;
+    }
+    public static void assign3(List<? extends Object> list){
+        list1 = list;
+        list2 = list;
+        list3 = list;
+    }
+    public static void main(String[] args){
+        List list = new ArrayList();
+        assign1(list);
+        assign2(list);
+        assign3(list);
+    }
+}
