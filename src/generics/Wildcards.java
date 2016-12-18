a8de0193a8b82d0a769f3abf17e4eb1d439b606c
cunkin1985 <anonymous.vn1985@gmail.com>
2016-12-18 10:42:53 +0700

    new commit

--- src/generics/Wildcards.java
+++ src/generics/Wildcards.java
@@ -30,7 +30,48 @@ public class Wildcards {
         // holder.setValue(arg); Error
         return holder.getValue();
     }
+    public static  <T> void wildSuperType(Holder<? super T> holder, T arg){
+        holder.setValue(arg);
+        // T t = holder.getValue(); Errof
+        Object object = holder.getValue(); // OK but type information is lost
+    }
     public static void main(String[] args){
+        Holder raw = new Holder<Long>();
+        // Or:
+        raw = new Holder();
+        Holder<Long> qualified = new Holder<Long>();
+        Holder<?> unbounded = new Holder<Long>();
+        Holder<? extends Long> bounded = new Holder<Long>();
+        Long lng = 1L;
 
+        rawArgs(raw, lng);
+        rawArgs(qualified, lng);
+        rawArgs(unbounded, lng);
+        rawArgs(bounded, lng);
+
+        unboundedArg(raw, lng);
+        unboundedArg(qualified, lng);
+        unboundedArg(unbounded, lng);
+        unboundedArg(bounded, lng);
+
+        Object r1 = exact1(raw);
+        Long r2 = exact1(qualified);
+        Object r3 = exact1(unbounded);
+        Long r4 = exact1(bounded);
+
+        Object r5 = exact2(raw, lng);
+        Long r6 = exact2(qualified, lng);
+        // Long r7 = exact2(unbounded, lng); Error
+        //Long r8 = exact2(bounded, lng); Error
+
+        Long r9 = wildSubType(raw, lng);
+        Long r10 = wildSubType(qualified, lng);
+        Object r11 = wildSubType(unbounded, lng);
+        Long r12 = wildSubType(bounded, lng);
+
+        wildSuperType(raw, lng);
+        wildSuperType(qualified, lng);
+        // wildSuperType(unbounded, lng);
+        // wildSuperType(bounded, lng);
     }
 }
