2144aabc91f950a31f519fa5b4301df5512221f5
cunkin1985 <anonymous.vn1985@gmail.com>
2017-01-01 18:15:38 +0700

    new commit

---
+++ src/review/GenericArray.java
@@ -0,0 +1,28 @@
+package review;
+
+/**
+ * Created by anonymous on 1/1/2017.
+ */
+public class GenericArray<T> {
+    private T[] array;
+    @SuppressWarnings("unchecked")
+    public GenericArray(int size){
+        // array = new T[size]; Error
+        array = (T[])new Object[size];
+    }
+    public void put(int index, T item){
+        array[index] = item;
+    }
+    public T get(int index){
+        return array[index];
+    }
+    public T[] rep(){
+        return array;
+    }
+    public static void main(String[] args){
+        GenericArray<Integer> genericArray = new GenericArray<>(10);
+        // ClassCastException will be thrown...
+        Integer[] integerArray = genericArray.rep();
+        Object[] objectArray = genericArray.rep();
+    }
+}
