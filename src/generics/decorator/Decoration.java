c8de361e5226a6b436df7f2a07fc95049a9a0c52
Dell Vostro 5459 <caoxuanquy1985@gmail.com>
2017-01-05 16:14:43 +0700

    new commit

--- src/generics/decorator/Decoration.java
+++ src/generics/decorator/Decoration.java
@@ -3,5 +3,14 @@ package generics.decorator;
 /**
  * Created by Dell Vostro 5459 on 1/5/2017.
  */
+import java.util.*;
+
+class Basic{}
+class Decorator extends Basic{}
+class TimeStamped extends Decorator{}
+class SerialNumbered extends Decorator{}
 public class Decoration {
+    public static void main(String[] args){
+
+    }
 }
