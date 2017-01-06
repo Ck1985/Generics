52fdb98d0c1d17cc97f468bf427f982b39bd0ef7
Dell Vostro 5459 <caoxuanquy1985@gmail.com>
2017-01-07 01:06:13 +0700

    new commit

--- src/generics/decorator/Decoration.java
+++ src/generics/decorator/Decoration.java
@@ -5,8 +5,27 @@ package generics.decorator;
  */
 import java.util.*;
 
-class Basic{}
-class Decorator extends Basic{}
+class Basic{
+    private String value;
+    public void set(String value){
+        this.value = value;
+    }
+    public String get(){
+        return this.value;
+    }
+}
+class Decorator extends Basic{
+    protected Basic basic;
+    public Decorator(Basic basic){
+        this.basic = basic;
+    }
+    public void set(String value){
+        this.basic.set(value);
+    }
+    public String get(){
+        return this.basic.get();
+    }
+}
 class TimeStamped extends Decorator{}
 class SerialNumbered extends Decorator{}
 public class Decoration {
