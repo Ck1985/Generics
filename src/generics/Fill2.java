d64bbb581c8bbfd009b1a01dd83482ecc6a932c3
cunkin1985 <anonymous.vn1985@gmail.com>
2017-01-19 17:03:02 +0700

    new commit

--- src/generics/Fill2.java
+++ src/generics/Fill2.java
@@ -21,12 +21,46 @@ public class Fill2 {
             }
         }
     }
+    // Generator version,
     public static <T> void fill(AddAble<T> addAble, Generator<T> generator, int size){
         for(int i = 0; i < size; i++){
             addAble.add(generator.next());
         }
     }
 }
+class AddAbleCollectionAdapter<T> implements AddAble<T>{
+    private Collection<T> collection;
+    public AddAbleCollectionAdapter(Collection<T> collection){
+        this.collection = collection;
+    }
+    public void add(T item){
+        this.collection.add(item);
+    }
+}
+class Adapter{
+    public static <T> AddAble<T> collectionAdapter(Collection<T> collection){
+        return new AddAbleCollectionAdapter<T>(collection);
+    }
+}
+class SimpleQueueAdapter<T> extends SimpleQueue<T> implements AddAble<T>{
+    public void add(T item){
+        super.add(item);
+    }
+}
 class Fill2Test{
-
+    public static void main(String[] args){
+        List<Coffee> carrier = new ArrayList<>();
+        Fill2.fill(new AddAbleCollectionAdapter<Coffee>(carrier), Coffee.class, 3);
+        Fill2.fill(Adapter.collectionAdapter(carrier), Latte.class, 3);
+        for(Coffee coffee : carrier){
+            System.out.print(coffee + " ");
+        }
+        System.out.println();
+        SimpleQueueAdapter<Coffee> simpleQueueAdapter = new SimpleQueueAdapter<Coffee>();
+        Fill2.fill(simpleQueueAdapter, Mocha.class, 3);
+        Fill2.fill(simpleQueueAdapter, Latte.class, 2);
+        for(Coffee coffee : simpleQueueAdapter){
+            System.out.println(coffee);
+        }
+    }
 }
