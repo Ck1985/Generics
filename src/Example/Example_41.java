04daebdb7225bdbeb2ad5a178430b31a9693ecb5
Dell Vostro 5459 <caoxuanquy1985@gmail.com>
2017-01-19 23:32:52 +0700

    new commit

--- src/Example/Example_41.java
+++ src/Example/Example_41.java
@@ -5,13 +5,14 @@ package Example;
  */
 import java.util.*;
 import generics.pets.*;
-import net.mindview.util.Generator.*;
+import generics.*;
+import net.mindview.util.Generator;
 
 interface AddAble<T>{
-    void add(T itam);
+    void add(T item);
 }
 class Fill{
-    public <T> void fill(AddAble<T> addAble, Class<? extends T> typeToken, int size){
+    public static <T> void fill(AddAble<T> addAble, Class<? extends T> typeToken, int size){
         for(int i = 0; i < size; i++){
             try {
                 addAble.add(typeToken.newInstance());
@@ -20,16 +21,45 @@ class Fill{
             }
         }
     }
-    public <T> void fill(AddAble<T> addAble, Generator<T>)
+    public static <T> void fill(AddAble<T> addAble, Generator<T> generator, int size){
+        for(int i = 0; i < size; i++){
+            addAble.add(generator.next());
+        }
+    }
 }
-class AddAbleCollectionAdapter{
-
+class AddAbleCollectionAdapter<T> implements AddAble<T>{
+    private Collection<T> collection;
+    public AddAbleCollectionAdapter(Collection<T> collection){
+        this.collection = collection;
+    }
+    public void add(T item){
+        this.collection.add(item);
+    }
 }
 class Adapter{
-
+    public static <T> AddAble<T> collectionAdapter(Collection<T> collection){
+        return new AddAbleCollectionAdapter<T>(collection);
+    }
+}
+class SimpleQueueAdapter<T> extends SimpleQueue<T> implements AddAble<T>{
+    public void add(T item){
+        super.add(item);
+    }
 }
 public class Example_41 {
     public static void main(String[] args){
-
+        List<Pet> pets = new ArrayList<>();
+        Fill.fill(new AddAbleCollectionAdapter<Pet>(pets), Dog.class, 3);
+        Fill.fill(Adapter.collectionAdapter(pets), Cat.class, 2);
+        for(Pet pet : pets){
+            System.out.println(pet);
+        }
+        System.out.println();
+        SimpleQueueAdapter<Pet> simpleQueueAdapter = new SimpleQueueAdapter<>();
+        Fill.fill(simpleQueueAdapter, Rat.class, 4);
+        Fill.fill(simpleQueueAdapter, Cymric.class, 3);
+        for(Pet pet : simpleQueueAdapter){
+            System.out.println(pet);
+        }
     }
 }
